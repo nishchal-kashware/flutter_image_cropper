3b6a2b194fcab3bd801c00cf54a3d445017295e4
nishchalappitron <nishchal@appitronsolutions.com>
2023-07-04 16:13:16 +0530

    http package version changes

--- lib/image_cropper.dart
+++ lib/image_cropper.dart
@@ -1,15 +1,15 @@
-// ///
-// /// * author: Hung Duy Ha (hunghd)
-// /// * email: hunghd.yb@gmail.com
-// ///
-// /// A plugin provides capability of manipulating an image including rotating
-// /// and cropping.
-// ///
-// /// Note that: this plugin is based on different native libraries depending on
-// /// Android or iOS platform, so it shows different UI look and feel between
-// /// those platforms.
-// ///
-//
+///
+/// * author: Hung Duy Ha (hunghd)
+/// * email: hunghd.yb@gmail.com
+///
+/// A plugin provides capability of manipulating an image including rotating
+/// and cropping.
+///
+/// Note that: this plugin is based on different native libraries depending on
+/// Android or iOS platform, so it shows different UI look and feel between
+/// those platforms.
+///
+
 // export 'package:image_cropper_platform_interface/image_cropper_platform_interface.dart'
 //     show
 //         CropAspectRatioPreset,
@@ -28,5 +28,5 @@
 //         CroppieViewPort,
 //         CroppieBoundary,
 //         RotationAngle;
-//
-// export 'src/cropper.dart';
+
+export 'src/cropper.dart';
